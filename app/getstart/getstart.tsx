0cb0f7354de3d5ba4d5b2d12ac6dacd3bbd5278b
Huynh Ba Anh <huynhbaanh03112003@@gmail.com>
2024-11-06 14:28:23 +0700

    done hotel detail

--- app/getstart/getstart.tsx
+++ app/getstart/getstart.tsx
@@ -11,48 +11,49 @@ import { AntDesign } from "@expo/vector-icons";
 
 export default function GetStart() {
     return (
-        // <SafeAreaView  >
-        <View style={styles.container}>
-            <View style={styles.skipContainer}>
-                <Link href='/login/login' asChild>
-                    <TouchableOpacity>
-                        <Text style={[styles.headerTeaxt]}> Skip
-                            <AntDesign name="right" size={24} color="black" />
-                        </Text>
-                    </TouchableOpacity>
-                </Link>
-
-            </View>
-            <View style={styles.getContainer}>
-                <View>
-                    <Text style={styles.title}>
-                        Get lost in the journey,
-                    </Text>
-                    <Text style={styles.title}>find yourself in the</Text>
-                    <Text style={styles.title}>Adventure.</Text>
-                </View>
-                <Image
-                    style={styles.image}
-                    source={start}
-                />
-                <View style={styles.btn}>
-                    <Link href='/getstart/adventures/adventures' asChild>
-                        <TouchableOpacity>
-                            <Text style={{ textAlign: "center" }} >GET STARTED</Text>
-                        </TouchableOpacity>
-                    </Link>
-                </View>
-
-                <View style={styles.header}>
-                    <Text style={{ fontSize: 20 }}>Already have an account ?</Text>
+        <SafeAreaView  >
+            <View style={styles.container}>
+                <View style={styles.skipContainer}>
                     <Link href='/login/login' asChild>
                         <TouchableOpacity>
-                            <Text style={{ fontSize: 20, color: "#00D38A" }} > Sign In</Text>
+                            <Text style={[styles.headerTeaxt]}> Skip
+                                <AntDesign name="right" size={24} color="black" />
+                            </Text>
                         </TouchableOpacity>
                     </Link>
+
+                </View>
+                <View style={styles.getContainer}>
+                    <View>
+                        <Text style={styles.title}>
+                            Get lost in the journey,
+                        </Text>
+                        <Text style={styles.title}>find yourself in the</Text>
+                        <Text style={styles.title}>Adventure.</Text>
+                    </View>
+                    <Image
+                        style={styles.image}
+                        source={start}
+                    />
+                    <View style={styles.btn}>
+                        <Link href='/getstart/adventures/adventures' asChild>
+                            <TouchableOpacity>
+                                <Text style={{ textAlign: "center" }} >GET STARTED</Text>
+                            </TouchableOpacity>
+                        </Link>
+                    </View>
+
+                    <View style={styles.header}>
+                        <Text style={{ fontSize: 20 }}>Already have an account ?</Text>
+                        <Link href='/login/login' asChild>
+                            <TouchableOpacity>
+                                <Text style={{ fontSize: 20, color: "#00D38A" }} > Sign In</Text>
+                            </TouchableOpacity>
+                        </Link>
+                    </View>
                 </View>
             </View>
-        // </SafeAreaView>
+        </SafeAreaView>
 
     );
 }
